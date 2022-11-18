f7871c7d23e3d8182e153151f20b0e7ba03168e8
walkingjunkie <112627569+walkingjunkie@users.noreply.github.com>
2022-11-19 01:05:42 +0900

    update

--- database/migrations/2022_11_18_231513_create_todos_table.php
+++ database/migrations/2022_11_18_231513_create_todos_table.php
@@ -13,7 +13,7 @@ class CreateTodosTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamp('created_at')->useCurrent()->nullable();
