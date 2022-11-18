e33e02d2d3239fa0e9c492b8f4246245c0bad474
walkingjunkie <112627569+walkingjunkie@users.noreply.github.com>
2022-11-19 07:29:47 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/todos',[TodoController::class,'index']);
+
+Route::resource('todos',[TodoController::class,'index']);
